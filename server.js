874e3c6cd830fd0ae899ab3ef7b7d288ad20c6c7
shvv733kn@gmail.com <shvv733kn@gmail.com>
2017-08-28 22:35:56 +0300

    epic_boss_r0ut3_fix

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+const fallback = require('express-history-api-fallback');
 const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+const root = __dirname + '/dist';
+app.use(express.static(root));
+app.use(fallback('index.html', { root }))
+
 // Start the app by listening on the default
 // Heroku port
 
